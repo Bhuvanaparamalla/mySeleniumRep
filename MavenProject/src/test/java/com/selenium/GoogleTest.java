1b523a2f4433b9106c363da1a60b33645fdcbe57
Bhuvanaparamalla <paramallabhuvana@gmail.com>
2021-07-12 15:19:30 +0530

    changed

---
+++ MavenProject/src/test/java/com/selenium/GoogleTest.java
@@ -0,0 +1,10 @@
+package com.selenium;
+
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+ @Test
+ public void googletest() {
+	 System.out.println("googletest");
+ }
+}
---
+++ MavenProject/src/test/java/com/selenium/YahooTest.java
@@ -0,0 +1,10 @@
+package com.selenium;
+
+import org.testng.annotations.Test;
+
+public class YahooTest {
+	@Test	
+	public void yahootest() {
+		System.out.println("yahoo test");
+	}
+	}
